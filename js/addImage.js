de64237052680204738bf4e6f7e4fbec2f82f061
Vi Snyder <ViTheAxolotl@gmail.com>
2024-09-05 14:37:03 -0400

    add image and match

---
+++ js/addImage.js
@@ -0,0 +1,52 @@
+"use strict";
+
+import { ref, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
+import { toTitleCase, auth, database, setDoc, deleteDoc, placeBefore } from './viMethods.js';
+
+/**
+ * Checks if user is logged in, if they aren't send them to loginPage
+ */
+onAuthStateChanged(auth, (user) => 
+{
+    if (!user) //If user isn't logged in
+    {
+        alert("You need to login before using this resource. Click Ok and be redirected");
+        window.location.href = "loginPage.html?match.html"; 
+    } 
+
+    else
+    {
+        player = auth.currentUser.email.split("@");
+        player = toTitleCase(player[0]);
+    }
+});
+
+let player;
+let name = document.getElementById("name");
+let category = document.getElementById("category");
+let img = document.getElementById("img");
+let sound = document.getElementById("sound");
+let submit = document.getElementById("submit")
+
+/**
+ * Runs when user is logged in
+ */
+function init()
+{
+    submit.onclick = handleSubmit;
+}
+
+function handleSubmit()
+{
+    let object = {
+        name : name.value,
+        src : img.value,
+        correct : false,
+        sound : sound.value
+    }
+    
+    setDoc(`${category.value}/${name}`, object);
+}
+
+init();
---
+++ js/match.js
@@ -0,0 +1,56 @@
+"use strict";
+
+import { ref, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
+import { toTitleCase, auth, database, setDoc, deleteDoc, placeBefore } from './viMethods.js';
+
+/**
+ * When anything under this changes it will use onValue
+ */
+const accountsRef = ref(database, 'Accounts/');
+onValue(accountsRef, (snapshot) => 
+{
+    const data = snapshot.val();
+    wholeAccounts = data;
+    
+    if(firstRun)
+    {
+        init();
+        firstRun = false;
+    }
+});
+
+/**
+ * Checks if user is logged in, if they aren't send them to loginPage
+ */
+onAuthStateChanged(auth, (user) => 
+{
+    if (!user) //If user isn't logged in
+    {
+        alert("You need to login before using this resource. Click Ok and be redirected");
+        window.location.href = "loginPage.html?match.html"; 
+    } 
+
+    else
+    {
+        player = auth.currentUser.email.split("@");
+        player = toTitleCase(player[0]);
+    }
+});
+
+let player;
+let wholeAccounts = {};
+let firstRun = true;
+let category = window.location.href;
+
+/**
+ * Runs when user is logged in
+ */
+function init()
+{
+    category = category.split("?");
+    category = category[1];
+    setDoc(`Accounts/${user}/category`, category);
+
+    "../sounds/household/toilet.wav"
+}
