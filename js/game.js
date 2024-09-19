b21ac1b74d76bc688946db43104e91e8654ab5fb
Vi Snyder <ViTheAxolotl@gmail.com>
2024-09-19 13:15:44 -0400

    ?

--- js/game.js
+++ js/game.js
@@ -117,7 +117,7 @@ function beginBasketBall()
     ball.src = "images/game/basketball.png";
     ball.ondragend = function (ev) {ev.preventDefault();};
 
-    div.parentElement.ondrop = function(e) {
+    div.ondrop = function(e) {
         if(e.target == document.getElementById('sweetSpot')) {
             alert("correct");
         } else {
