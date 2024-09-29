9f57e97bc8c1e514418d8f4e3bdb6e9501cf2322
Vi Snyder <ViTheAxolotl@gmail.com>
2024-09-28 22:07:03 -0400

    ?

--- js/game.js
+++ js/game.js
@@ -112,7 +112,7 @@ function beginBasketBall()
         let elm = document.createElement("img");
         elm.id = label[i];
         elm.src = `images/game/${label[i]}.png`;
-        if(i == 0 || i == 2){elm.style.zIndex = "100";}
+        if(i == 0){elm.style.zIndex = "100";}
         else if(i > 0){elm.classList.add("overlap"); elm.classList.add("hoop");}
         gameDiv.appendChild(elm);
     }
